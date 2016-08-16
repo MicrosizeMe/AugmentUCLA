0a57d5fc5829d9354c77d8860179e85cca5f40e8
MicrosizeMe <anbo_wei@outlook.com>
2016-08-16 13:06:24 -0700

    Carousel "works," but isn't using a component and isn't consistent. Navbar now links to merch as well.

--- webpages/js/merchApp.js
+++ webpages/js/merchApp.js
@@ -6,6 +6,39 @@
 		'carouselController'
 	]);
 
+	app.controller('CarouselController', ["$http", "$location",  function($http, $location) {
+		//Get which team we're supposed to be pulling the data from
+		var team = $location.search().team;
+
+		console.log("carousel");
+
+		//Get carousel data somehow
+		this.slides = [
+			{
+				imageSrc: "http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg",
+				imageCaption: "I should buy a boat!"
+			},
+			{
+				imageSrc: "http://www.planwallpaper.com/static/images/techno_wallpaper_2_0_hd_by_gredius-d5o48do.jpg",
+				imageCaption: "Starrrs"
+			},
+			{
+				imageSrc: "http://www.planwallpaper.com/static/images/Desktop-Wallpaper-HD2.jpg",
+				imageCaption: "Balooooons"
+			},
+			{
+				imageSrc: "http://www.intrawallpaper.com/static/images/wallpaper__eeveelution_rainbow_by_arkeis_pokemon-d49kjyc.jpg",
+				imageCaption: "What a nerd!"
+			}
+		];
+
+		//Set the first picture to active. 
+		this.getActive = function(index) {
+			return (index === 0) ? "active" : "";
+		};
+
+	}]);
+
 	app.controller('ShopController', ['$http', '$location', '$sce', 
 		function($http, $location, $sce) {
 			//Get shop info somehow
