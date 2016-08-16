4ac67c1c80f49ad801b9c18868d96863a5d1b8b1
MicrosizeMe <anbo_wei@outlook.com>
2016-08-16 13:51:56 -0700

    Not worth making a carousel a separate component, simply moving controller to separate file and doing dom manipulation inside files

--- webpages/js/aboutApp.js
+++ webpages/js/aboutApp.js
@@ -2,39 +2,39 @@
 	var app = angular.module('AboutPage', [
 		'headerController',
 		'looseDirectives',
-		// 'carouselController'
+		'carouselController'
 	]);
 
-	app.controller('CarouselController', ["$http", "$location",  function($http, $location) {
-		//Get which team we're supposed to be pulling the data from
-		var team = $location.search().team;
+	// app.controller('CarouselController', ["$http", "$location",  function($http, $location) {
+	// 	//Get which team we're supposed to be pulling the data from
+	// 	var team = $location.search().team;
 
-		//Get carousel data somehow
-		this.slides = [
-			{
-				imageSrc: "http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg",
-				imageCaption: "I should buy a boat!"
-			},
-			{
-				imageSrc: "http://www.planwallpaper.com/static/images/techno_wallpaper_2_0_hd_by_gredius-d5o48do.jpg",
-				imageCaption: "Starrrs"
-			},
-			{
-				imageSrc: "http://www.planwallpaper.com/static/images/Desktop-Wallpaper-HD2.jpg",
-				imageCaption: "Balooooons"
-			},
-			{
-				imageSrc: "http://www.intrawallpaper.com/static/images/wallpaper__eeveelution_rainbow_by_arkeis_pokemon-d49kjyc.jpg",
-				imageCaption: "What a nerd!"
-			}
-		];
+	// 	//Get carousel data somehow
+	// 	this.slides = [
+	// 		{
+	// 			imageSrc: "http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg",
+	// 			imageCaption: "I should buy a boat!"
+	// 		},
+	// 		{
+	// 			imageSrc: "http://www.planwallpaper.com/static/images/techno_wallpaper_2_0_hd_by_gredius-d5o48do.jpg",
+	// 			imageCaption: "Starrrs"
+	// 		},
+	// 		{
+	// 			imageSrc: "http://www.planwallpaper.com/static/images/Desktop-Wallpaper-HD2.jpg",
+	// 			imageCaption: "Balooooons"
+	// 		},
+	// 		{
+	// 			imageSrc: "http://www.intrawallpaper.com/static/images/wallpaper__eeveelution_rainbow_by_arkeis_pokemon-d49kjyc.jpg",
+	// 			imageCaption: "What a nerd!"
+	// 		}
+	// 	];
 
-		//Set the first picture to active. 
-		this.getActive = function(index) {
-			return (index === 0) ? "active" : "";
-		};
+	// 	//Set the first picture to active. 
+	// 	this.getActive = function(index) {
+	// 		return (index === 0) ? "active" : "";
+	// 	};
 
-	}]);
+	// }]);
 
 	app.controller('DescriptionHandler', ["$http", "$location", function($http, $location) {
 		//Store the specific team we should be pulling data from
