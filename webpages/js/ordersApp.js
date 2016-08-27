3e58cc5b54f863183ef93844ea26c6f416ca42b1
MicrosizeMe <anbo_wei@outlook.com>
2016-08-26 17:41:40 -0700

    Order started

--- webpages/js/ordersApp.js
+++ webpages/js/ordersApp.js
@@ -11,10 +11,126 @@
 		}
 	);
 
-	app.controller('OrdersController', ["$http", "$location",
-		function($http, $location) {
+	app.controller('OrdersController', ["$http", "$location", "$sce",
+		function($http, $location, $sce) {
 			//Get personal info somehow
-			this.obtainedFirstName = "Anbo";
+
+			//Get order history somehow
+			this.pendingOrders = [
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				},
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				},
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				}
+			];
+
+			this.completedOrders = [
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				},
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				},
+				{
+					orderNumber: 1234,
+					delivery: 7498294,
+					orderDate: 732911,
+					items: [
+						{
+							itemId: 17894,
+							quantity: 1
+						},
+						{
+							itemId: 17894,
+							quantity: 1
+						},	
+					]
+				}
+			];
+
+			//Dictionary of items and such to be populated by ID
+			this.itemList = {};
+
+			//Populate a list of item numbers from these orders, then look them up
+			for (var i = 0; i < this.pendingOrders.length; i++) {
+				var currentOrder = this.pendingOrders[i];
+				for (var j = 0; j < currentOrder.items.length; j++) {
+					currentItem = currentOrder.items[j].itemId;
+					if (!this.itemList.hasOwnProperty(currentItem)) {
+						this.itemList[currentItem] = {
+							id: 17894,
+							image: "http://megaicons.net/static/img/icons_sizes/7/59/64/pixelmator-icon.png",
+							name: "Augment Membership",
+							price: 5.00,
+							shortDescription: $sce.trustAsHtml("<p>Access to all of our wonderful club events for the entire year! This pays tournament costs, something, yada, gib moneys. </p>") 
+						};
+					}
+				}
+			}
 		}
 	]);
 }());
