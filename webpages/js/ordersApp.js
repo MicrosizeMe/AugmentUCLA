04331567350610e1a49332ff50e44828024f560e
MicrosizeMe <microsizeme@outlook.com>
2016-08-28 02:20:59 -0700

    Order form front end done?

--- webpages/js/ordersApp.js
+++ webpages/js/ordersApp.js
@@ -28,7 +28,7 @@
 						},
 						{
 							itemId: 17894,
-							quantity: 1
+							quantity: 2
 						},	
 					]
 				},
@@ -115,22 +115,38 @@
 			//Dictionary of items and such to be populated by ID
 			this.itemList = {};
 
+			var controller = this;
 			//Populate a list of item numbers from these orders, then look them up
-			for (var i = 0; i < this.pendingOrders.length; i++) {
-				var currentOrder = this.pendingOrders[i];
-				for (var j = 0; j < currentOrder.items.length; j++) {
-					currentItem = currentOrder.items[j].itemId;
-					if (!this.itemList.hasOwnProperty(currentItem)) {
-						this.itemList[currentItem] = {
-							id: 17894,
-							image: "http://megaicons.net/static/img/icons_sizes/7/59/64/pixelmator-icon.png",
-							name: "Augment Membership",
-							price: 5.00,
-							shortDescription: $sce.trustAsHtml("<p>Access to all of our wonderful club events for the entire year! This pays tournament costs, something, yada, gib moneys. </p>") 
-						};
+			var populateList = function(list) {
+				for (var i = 0; i < list.length; i++) {
+					var currentOrder = list[i];
+					currentOrder.price = 0;
+					for (var j = 0; j < currentOrder.items.length; j++) {
+						currentItem = currentOrder.items[j].itemId;
+						if (!controller.itemList.hasOwnProperty(currentItem)) {
+							controller.itemList[currentItem] = {
+								id: 17894,
+								image: "http://megaicons.net/static/img/icons_sizes/7/59/64/pixelmator-icon.png",
+								name: "Augment Membership",
+								price: 5.00,
+								shortDescription: $sce.trustAsHtml("<p>Access to all of our wonderful club events for the entire year! This pays tournament costs, something, yada, gib moneys. </p>") 
+							};
+						}
+						currentOrder.price += currentOrder.items[j].quantity * controller.itemList[currentItem].price
 					}
 				}
-			}
+			};
+
+			populateList(this.pendingOrders);
+			populateList(this.completedOrders);
+
+
+			//Delete a pending order
+			this.deletePending = function(index) {
+				if (confirm("Are you sure? This will cancel the order.")) {
+					this.pendingOrders.splice(index, 1);
+				}
+			};
 		}
 	]);
 }());
