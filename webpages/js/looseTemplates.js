6ba0b922ba0f1646ccd6dc96315b6a1a6fdfcf77
Anbo Wei <anbo_wei@outlook.com>
2016-12-21 02:52:15 -0500

    No real changes, but documenting that templating should follow, then
    email

--- webpages/js/looseTemplates.js
+++ webpages/js/looseTemplates.js
@@ -1,13 +1,6 @@
 (function() {
 	var app = angular.module('looseDirectives', []);
 
-	// app.directive("footerAndCopyright", function() {
-	// 	return {
-	// 		restrict: "E",
-	// 		templateUrl: "/htmltemplates/footer-and-copyright.html"
-	// 	};
-	// });
-
 	app.directive("redAsterisk", function() {
 		return {
 			restrict: 'E',
