792827ad63bab978efab7005d771d4406d3a2a18
Anbo Wei <anbo_wei@outlook.com>
2017-01-04 06:15:08 -0500

    Removed testing data

--- webpages/js/calendarApp.js
+++ webpages/js/calendarApp.js
@@ -26,114 +26,114 @@
 			return returnString;
 		}
 
-		// this.calendarData = {
-		// 	id: null,
-		// 	title: null,
-		// 	iframeURL: null,
-		// 	events: []
-		// };
-
 		this.calendarData = {
-			id: "augment",
-			title: "UCLA Augment or whatever",
-			iframeURL: "https://calendar.google.com/calendar/embed?src=acmbruins%40gmail.com&ctz=America/Los_Angeles",
-			events: [
-				{
-					id: "test",
-					title: "test",
-					startDate: new Date(2011, 8, 1),
-					endDate: new Date(2011, 9, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test3",
-					title: "test2",
-					startDate: new Date(2020, 9, 1),
-					endDate: new Date(2020, 10, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test2",
-					title: "test2",
-					startDate: new Date(2020, 8, 1),
-					endDate: new Date(2020, 9, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test4",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test5",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test6",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test7",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test8",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test9",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				},
-				{
-					id: "test10",
-					title: "test2",
-					startDate: new Date(2020, 10, 1),
-					endDate: new Date(2020, 11, 1),
-					aboutPageId: "augment",
-					shortDescription: $sce.trustAsHtml("Testing!")
-				}
-			]
+			id: null,
+			title: null,
+			iframeURL: null,
+			events: []
 		};
 
-		// databaseInfo.then(function(returnRequest) {
-		// 	if (returnRequest.data == null || returnRequest.data.error != null) {
-		// 		scope.calendarData.title = "Error: " + returnRequest.error;
-		// 	}
-		// 	else {
-		// 		scope.calendarData = returnRequest.data;
-		// 		scope.calendarData.iframeURL = $sce.trustAsResourceUrl(scope.calendarData.iframeURL);
-		// 		for (var i = 0; i < scope.calendarData.events.length; i++) {
-		// 			var description = scope.calendarData.events[i].shortDescription;
-		// 			description = $sce.trustAsHtml(description);
+		// this.calendarData = {
+		// 	id: "augment",
+		// 	title: "UCLA Augment or whatever",
+		// 	iframeURL: "https://calendar.google.com/calendar/embed?src=acmbruins%40gmail.com&ctz=America/Los_Angeles",
+		// 	events: [
+		// 		{
+		// 			id: "test",
+		// 			title: "test",
+		// 			startDate: new Date(2011, 8, 1),
+		// 			endDate: new Date(2011, 9, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test3",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 9, 1),
+		// 			endDate: new Date(2020, 10, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test2",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 8, 1),
+		// 			endDate: new Date(2020, 9, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test4",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test5",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test6",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test7",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test8",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test9",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
+		// 		},
+		// 		{
+		// 			id: "test10",
+		// 			title: "test2",
+		// 			startDate: new Date(2020, 10, 1),
+		// 			endDate: new Date(2020, 11, 1),
+		// 			aboutPageId: "augment",
+		// 			shortDescription: $sce.trustAsHtml("Testing!")
 		// 		}
-		// 	}
-		// 	console.log(scope.calendarData);
-		// });
+		// 	]
+		// };
+
+		databaseInfo.then(function(returnRequest) {
+			if (returnRequest.data == null || returnRequest.data.error != null) {
+				scope.calendarData.title = "Error: " + returnRequest.error;
+			}
+			else {
+				scope.calendarData = returnRequest.data;
+				scope.calendarData.iframeURL = $sce.trustAsResourceUrl(scope.calendarData.iframeURL);
+				for (var i = 0; i < scope.calendarData.events.length; i++) {
+					var description = scope.calendarData.events[i].shortDescription;
+					description = $sce.trustAsHtml(description);
+				}
+			}
+			console.log(scope.calendarData);
+		});
 	}]);
 })();
